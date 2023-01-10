c99374b8e18510a423e9daecd49a1c444867f8e9
German Gauna <germangauna88@gmail.com>
2023-01-09 21:01:39 -0300

    creamos las carpetas ItemListContainer/ItemCount

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,17 +1,33 @@
+import React,{ useEffect, useState } from 'react';
 import './itemCount.css';
 
-import React from "react";
+
 
 // creamos el contador para las card pra agregar al carrito
 
-export const ItemCount = () => {
+export const ItemCount = ({initial, stock, onAdd}) => {
+
+    const [count, setCount] = useState(parseInt(initial));// creamos el count para el contador, a traves de los props
+
+    const decrease = () => {
+        setCount(count - 1);//seteamos para que disminuya y al boton
+    }
+
+    const increase = () => {
+        setCount(count + 1);//seteamos para que incremente y al boton
+    }
+
+    useEffect(() => {
+        setCount(parseInt(initial))//usamos el useEffect para setiar el valor initial
+    }, [initial])
+
     return (
         <div className="counter">
-            <button>-</button>
-            <span>2</span>
-            <button>+</button>
+            <button className='btn btn-outline' disabled={count <= 1} onClick={decrease}>-</button>
+            <span>{count}</span>
+            <button className='btn btn-outline' disabled={count >= stock} onClick={increase}>+</button>
             <div>
-                <button>agregar al carrito</button>
+                <button className='btn btn-outline' disabled={stock <= 0} onClick={() => onAdd(count)}>agregar al carrito</button>
             </div>      
         </div>
     );
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -3,11 +3,22 @@ import ItemCount from "../ItemCount"
 import Title from '../Title'
 
 
-const ItemListContainer = (props) => {
+
+
+
+
+
+//en el itemCount colocamos hasta donde puede ir el contador, con el onAdd mostramos lo que compra el cliente
+const ItemListContainer = ({texto}) => {
+
+  const onAdd = (quantity) => {
+    console.log(`compraste ${quantity} unidades`);
+  }
+
   return (
     <>
-      <Title greeting={props.texto} />
-      <ItemCount/>
+      <Title greeting={texto} />
+      <ItemCount initial={3} stock={5} onAdd={onAdd} />
     </>
     
   )
